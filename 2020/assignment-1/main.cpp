2e64b9c219e863e0e50241a30fcf931538912b6c
bashtag <btstsiy@hotmail.com>
2022-05-17 15:26:16 +0300

    assignment-1 main file uploaded

---
+++ 2020/assignment-1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "lib.h"
+
+using namespace std;
+
+int	main(void)
+{
+	vector<vector<Cell>> board;
+	int	boardSelection, gameSelection;
+
+	do
+	{
+		cout << "Give me a board type from 1 to 6: ";
+		cin >> boardSelection;
+		
+		if (1 <= boardSelection && boardSelection <= 6)
+		{
+			board = createBoard(boardSelection);	
+		}
+		else
+			cout << "Please try again..." << endl;
+	} while (1 > boardSelection || boardSelection > 6);
+
+	do
+	{
+		cout << "Select the game type: " << endl <<
+			"Human Player Game (0)" << endl <<
+			"Computer Game (1)" << endl <<
+			"Your choice: ";
+		cin >> gameSelection;
+
+		if (gameSelection == 1 || gameSelection == 0)
+			switch (gameSelection)
+			{
+				case 0:
+					humanPlayerGame(board, boardSelection);
+					break;
+				
+				case 1:
+					srand(time(NULL));
+					computerGame(board, boardSelection);
+				default:
+					break;
+			}
+		else
+			cout << "That wasn't a valid game type!" << endl;
+	} while (gameSelection != 1 && gameSelection != 0);
+	
+	return (0);
+}
