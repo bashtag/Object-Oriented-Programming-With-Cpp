7b7b2734f89bdf1990bab039fbfa313a7f0ba513
bashtag <btstsiy@hotmail.com>
2022-10-05 19:30:31 +0300

    boardGame header

--- 2020/assignment-5/BoardGame2D.h
+++ 2020/assignment-5/BoardGame2D.h
@@ -2,6 +2,7 @@
 #define _BOARDGAME2D_H_
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,13 +15,63 @@ namespace bg2d
 	class BoardGame2D
 	{
 	private:
+		int	bScore;
 	public:
 		BoardGame2D();
-		~BoardGame2D();
 
-		void	playUser(string);
+		/* pure virtual function (= 0)*/
+
+		/**
+		 * @brief it takes a string and plays game
+		 * 
+		 */
+		virtual void	playUser(string) = 0;
+		/**
+		 * @brief it takes a string 
+		 * from the user for the next move in a loop and plays the game until it is over. It also prints the board 
+		 * between the moves.
+		 * 
+		 */
 		virtual void	playUser() final;
+		/**
+		 * @brief it plays the game by the computer for one move
+		 * 
+		 */
+		virtual void	playAuto() = 0;
+		/**
+		 * @brief It plays the game until it is over. 
+		 * This one calls playAuto for all the moves.
+		 * It also prints the board between the moves after some pause.
+		 * 
+		 */
+		virtual void	playAutoAll() final;
+		/**
+		 * @brief prints the game on the screen
+		 * starting from the top left corner of the terminal.
+		 * 
+		 */
+		virtual void	print() = 0;
+		friend ostream&	operator<<(ostream& output, const BoardGame2D&);
 
+		/**
+		 * @brief it returns true if the game is ended
+		 * 
+		 * @return true 
+		 * @return false 
+		 */
+		virtual bool	endGame() = 0;
+		/**
+		 * @brief score value.
+		 * If the game is finished, it returns 0, which is the best case.
+		 * 
+		 */
+		virtual int	boardScore() const = 0;
+		virtual void	initialize() = 0;
+		/**
+		 * @brief it plays all the games in the vector
+		 * 
+		 */
+		static 	void	playVector(vector<BoardGame2D*>);
 	};
 }
 
