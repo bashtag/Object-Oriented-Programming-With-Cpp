390f401381515b6913010ffcfb9b4008f24aff44
bashtag <btstsiy@hotmail.com>
2022-10-17 13:49:42 +0300

    klotski declaretions

--- 2020/assignment-5/Klotski.h
+++ 2020/assignment-5/Klotski.h
@@ -7,18 +7,20 @@ namespace bg2d
 		public:
 			virtual void	playUser(string);
 			virtual void	playAuto();
-			virtual bool	endGame();
+			virtual bool	endGame() const;
 			virtual int	boardScore() const;
 			virtual void	initialize();
 			virtual void	gameInfo() const;
 			virtual void	print() const;
+			virtual void	writeFile();
+			virtual void	setBoardType(int);
 
 			Klotski();
-			void	randValidMove();
-			void	makeMove();
-			bool	isValidMove();
+			void	randValidMove(char&, string&);
+			void	makeMove(char, string);
+			bool	isValidMove(char, string) const;
 
 		private:
-			vector<vector<int>>	board;
+			vector<vector<char>>	board;
 	};
 }
