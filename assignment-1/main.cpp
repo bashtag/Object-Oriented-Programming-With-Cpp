c14bf0dc7783e90576060e29cdbe72bb1caa9450
bashtag <btstsiy@hotmail.com>
2022-11-21 15:05:43 +0300

    main driver code

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 #include "Tetromino.hpp"
+#include "Tetris.hpp"
+#include <thread>
+#include <chrono>
 
 using namespace	std;
 
 int	main(void)
 {
+	Tetris	tetrisGame;
+
+	tetrisGame.takeTetrominos();
+
+	cout << "Your tetrominos:" << endl;
+
+	tetrisGame.printTetrominos();
+
+	cout << "Horiztontally best-fit tetrominos" << endl;
+
+	tetrisGame.horizontallyBestFit();
+
+	tetrisGame.printBoard();
 
 	return (0);
 }
